37e76171e4cba3aa2018527587209ce7e4dbad95
Marine <marinekislova@gmail.com>
2024-10-09 13:30:02 +0300

    1. added popUp 2. realised emergence close button after open popup 3. realised disappearing popup after open 4. realised remove scroll after first scrolling

---
+++ modal_window/js/pop_up.js
@@ -0,0 +1,69 @@
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  function popUpWindow({ closeSelector, parentSelector }) {
+    // const modalTimerClose = setTimeout(
+    //   () => hideModal(parentSelector, modalTimerClose),
+    //   5000
+    // );
+
+    let closeModal = document.querySelectorAll(closeSelector); //закомментировано для динамического формирования
+    let parentModal = document.querySelector(parentSelector);
+
+    function showModal() {
+      parentModal.style.display = "block";
+    }
+
+
+    function hideModal() {
+      parentModal.style.display = "none";
+    }
+
+    closeModal.forEach((item) => {
+      item.style.opacity = 0;
+      setTimeout(() => {
+        item.style.opacity = 0.8;
+      }, 5000); //emergence close button after open popup
+      item.addEventListener("click", hideModal);
+    });
+
+    parentModal.addEventListener("click", (e) => {
+      if (e.target === parentModal) {
+        
+        hideModal();
+      }
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Escape") {
+        hideModal();
+        
+      }
+    });
+
+    function showByScroll() {
+      let offsetTop = parentModal.offsetTop;
+      if (offsetTop + scrollY >= 200) {
+        const modalTimerId = setTimeout(
+          () => showModal(parentSelector, modalTimerId),
+          1000
+        );
+
+        // disappearing popup after open
+        // const modalTimerClose = setTimeout(
+        //   () => hideModal(parentSelector, modalTimerClose),
+        //   5000
+        // );
+
+        window.removeEventListener("scroll", showByScroll);
+      }
+    }
+
+    window.addEventListener("scroll", showByScroll);
+  }
+
+  popUpWindow({
+    closeSelector: "[data-closemodal]",
+    parentSelector: ".modal-window",
+  });
+});
